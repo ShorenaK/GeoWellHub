2c780070edabdcae01c0bb2acac28a5e875017ea
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-06-09 18:38:42 -0400

    node man added on package jason

--- backend.js
+++ backend.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 console.log("Starting GeoWell Hub backend...");
 
+app.use(0x2f, express.static("frontend"));
+
 app.listen(PORT, () => {
     console.log(`GeoWell Hub backend is running on port ${PORT}`);
 });
