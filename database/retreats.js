5e15abb19ec3500b04609065252e0426cd1d65b3
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-06-10 13:25:39 -0400

    js retreats.js bulding processes

--- database/retreats.js
+++ database/retreats.js
@@ -1,2 +1,11 @@
 // Import the getDatabase helper from our MongoDB connection file
 import { getDatabase } from "./mongo.js";
+
+// Name of the MongoDB collection we will use for retreat listings
+const COLLECTION_NAME = "retreats";
+
+// Get all retreat listings from the database--- might change this later will see 
+export async function getRetreats() {
+  try {
+    // Get the connected database
+    const db = getDatabase();
